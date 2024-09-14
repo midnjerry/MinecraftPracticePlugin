26011ee025f4dfd54aed5512a321e363d21411b2
NoobDad <mardaed10@gmail.com>
2024-09-14 09:22:33 -0500

    Setup

---
+++ src/main/java/net/crusadergames/minecraft/plugins/practice/MinecraftPracticePlugin.java
@@ -0,0 +1,17 @@
+package net.crusadergames.minecraft.plugins.practice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftPracticePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
